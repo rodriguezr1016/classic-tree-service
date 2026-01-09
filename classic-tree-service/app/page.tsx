467f2dbca0daefb57a137ad68b34b995bf92a74f
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2026-01-09 09:44:03 -0800

    implemented edgestore

--- classic-tree-service/app/page.tsx
+++ classic-tree-service/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Looking for a Hello point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
@@ -48,7 +48,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Deploy Now
+            Deploy
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
