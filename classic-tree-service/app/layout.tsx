6ca9f982a1c101e8e115b22d624f16ebb7035122
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2026-01-22 10:11:01 -0800

    added metadata, changed layout.tsx to be a server component

--- classic-tree-service/app/layout.tsx
+++ classic-tree-service/app/layout.tsx
@@ -1,10 +1,20 @@
-"use client"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { EdgeStoreProvider } from '../lib/edgestore';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ClientProviders from "./ClientProviders";
+
+export const metadata: Metadata = {
+  title: 'Classic Tree Service | Tree Trimming & Removal in Modesto, CA | Tree Care Experts | Serving Modesto, Merced, & Surrounding Areas | Classic Tree Service',
+  description: 'Classic Tree Service offers expert tree trimming, tree removal, and stump grinding across multiple locations in California including Modesto and surrounding areas. Licensed, insured, and reliable. Free estimates available!',
+  openGraph: {
+    title: 'Classic Tree Service | Tree Trimming & Removal in Modesto, CA | Tree Care Experts | Serving Modesto, Merced, & Surrounding Areas | Classic Tree Service',
+    description: 'Classic Tree Service offers expert tree trimming, tree removal, and stump grinding across multiple locations in California including Modesto and surrounding areas. Licensed, insured, and reliable. Free estimates available!',
+    url: 'https://www.classic-tree-service.com',
+    siteName: 'Classic Tree Service',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -14,11 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <EdgeStoreProvider>
+        <ClientProviders>
         <Header />
         {children}
         <Footer />
-        </EdgeStoreProvider>
+        </ClientProviders>
       </body>
     </html>
   );
